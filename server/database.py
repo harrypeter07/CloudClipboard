2299a89fb3185f3e46016106d737cb3f7b82ff85
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-10-24 21:31:00 +0530

    f

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 
 # MongoDB connection string - replace <db_password> with your actual password
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://hassanmansuri570_db_user:mM5sBd36HUPNarMX@cluster0.8a1u4xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://hassanmansuri570_db_user:hassan@cluster0.8a1u4xz.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = "cloudclipboard"
 
 client = AsyncIOMotorClient(MONGODB_URL)
